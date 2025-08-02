cbd81450c956ff4faf4838f39da27d6ff4d6422f
Anirudh <157978614+Anirudh0709@users.noreply.github.com>
2025-08-02 11:42:17 +0530

    Time: 114 ms (17.53%) | Memory: 43.1 MB (74.67%) - LeetSync

---
+++ 2689-rearranging-fruits/rearranging-fruits.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def minCost(self, basket1: List[int], basket2: List[int]) -> int:
+        fruit_counter = Counter()
+        for fruit_type_a, fruit_type_b in zip(basket1, basket2):
+            fruit_counter[fruit_type_a] += 1
+            fruit_counter[fruit_type_b] -= 1
+
+        min_fruit_type = min(fruit_counter)
+        exchange_list = []
+
+        for fruit_type, count in fruit_counter.items():
+            if count % 2:
+                return -1
+            exchange_list.extend([fruit_type] * (abs(count) // 2))
+
+        exchange_list.sort()
+        mid_point = len(exchange_list) // 2
+        return sum(min(fruit_type, min_fruit_type * 2) for fruit_type in exchange_list[:mid_point])
