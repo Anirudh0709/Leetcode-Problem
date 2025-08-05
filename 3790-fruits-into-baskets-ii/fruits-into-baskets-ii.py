7194154b4bf95723562354b9625dd3a40591e6f3
Anirudh <157978614+Anirudh0709@users.noreply.github.com>
2025-08-05 10:47:17 +0530

    Time: 51 ms (8.58%) | Memory: 17.7 MB (96.47%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -3,7 +3,6 @@ class SegmentTree:
         self.n = len(nums)
         self.tree = [0] * (self.n * 4)
         self.build(nums, 0, 0, self.n - 1)
-
     def build(self, nums, index, lo, hi):
         if lo == hi:
             self.tree[index] = nums[lo]
@@ -12,10 +11,8 @@ class SegmentTree:
         self.build(nums, 2 * index + 1, lo, mid)
         self.build(nums, 2 * index + 2, mid + 1, hi)
         self.tree[index] = max(self.tree[2 * index + 1], self.tree[2 * index + 2])
-
     def update(self, i, val):
         self._update(0, 0, self.n - 1, i, val)
-
     def _update(self, index, lo, hi, i, val):
         if lo == hi:
             self.tree[index] = val
@@ -26,10 +23,8 @@ class SegmentTree:
         else:
             self._update(2 * index + 2, mid + 1, hi, i, val)
         self.tree[index] = max(self.tree[2 * index + 1], self.tree[2 * index + 2])
-
     def query_first(self, target):
         return self._query_first(0, 0, self.n - 1, target)
-
     def _query_first(self, index, lo, hi, target):
         if self.tree[index] < target:
             return -1
@@ -41,8 +36,6 @@ class SegmentTree:
             return self._query_first(2 * index + 1, lo, mid, target)
         else:
             return self._query_first(2 * index + 2, mid + 1, hi, target)
-
-
 class Solution:
     def numOfUnplacedFruits(self, fruits, baskets):
         ans = 0
