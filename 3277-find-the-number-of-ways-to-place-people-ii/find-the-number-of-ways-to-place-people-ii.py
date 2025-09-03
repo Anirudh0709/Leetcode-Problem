b01e625a74339a400873f6c3178358b27bbbcaad
Anirudh <157978614+Anirudh0709@users.noreply.github.com>
2025-09-03 18:02:18 +0530

    Time: 1040 ms (84.31%) | Memory: 18.2 MB (82.35%) - LeetSync

--- 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.py
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.py
@@ -3,12 +3,12 @@ class Solution:
         P.sort(key=lambda p:(-p[0], p[1]))
         ans, n=0, len(P)
         for i in range(n-1):
-            y, yi=1<<31, P[i][1]
+            a, ai=1<<31, P[i][1]
             for j in range(i+1, n):
-                yj=P[j][1]
-                if y>yj>=yi:
+                aj=P[j][1]
+                if a>aj>=ai:
                     ans+=1
-                    y=yj
-                    if yi==yj: break
+                    a=aj
+                    if ai==aj: break
         return ans
         
